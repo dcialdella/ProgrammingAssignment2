37bec429cc54beb97946dd83be940ef426b1e620
dcialdella <dcialdella@gmail.com>
2016-06-14 15:41:05 +0200

    look boog.... more testing still pending.
    matrix should be equal COL and ROWS ?

--- cachematrix.R
+++ cachematrix.R
@@ -12,30 +12,31 @@
 ## 
 makeCacheMatrix <- function( x = matrix() ) 
 {
-  m <- NULL
+  m <- NULL    # matrix INVERTED var to NULL
 
-  set<-function( y ) # set x with matrix  and  blank to Imatrix (m)
+  set<-function( y ) # set x = matrix passed to func (y)   and  m = NULL (inverted)
   {
     x <- y     
     m <- NULL
   }
 
-  get <- function()   # return matrix passed to function (x)
+  get <- function()   # return matrix passed to (x) - trivial
   {
     x
   }
 
-  setInverse <- function() 
+  setInverse <- function(inverse)   # ontain inverso y retorna
   {
+  	retorno <<- inverse
+  	retorno
   }
 
-  getInverse <- function()  # return the inverse 
+  getInverse <- function()  # return the inverse var (m)
   {
   	m
   }
 
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-
 }
 
 
@@ -47,14 +48,19 @@ makeCacheMatrix <- function( x = matrix() )
 
 cacheSolve <- function(x, ...) 
 {
-  m <- x$getInverse()
-  if ( is.null( m ) ) 
+  m <- x$getInverse()     # Obtain inverted temporary storage
+  if ( is.null( m ) )     # if null then calculate it
   {
     temporal <- x$get()
-    m        <- solve(temporal) %*% temporal
-    x$setInverse(m)
+    m        <- solve(temporal) %*% temporal   # the real vudu magic
+
+    # others use
+    # zz <- svd(temporal)
+    # m <- zz$v%*%diag(1/zz$d)%*%t(zz$u)
+
+    x$setInverse(m)       # store inverted in var (m)
   }
-  m # return matrix inverted
+  m                       # return matrix inverted
 }
 
 
@@ -91,6 +97,71 @@ How test it
   cacheSolve( func_process )    ## WARNING SINGULAR 
   inverso <- cacheSolve( func_process )
   inverso %*% x
+
+
+> set.seed(25) # funny number for this sample
+> x <- matrix( sample(3000), 5, 5)
+> func_process <- makeCacheMatrix(x)
+
+> func_process$get()   # Read cachematrix complete
+     [,1] [,2] [,3] [,4] [,5]
+[1,] 1249 2951  981  441  122
+[2,] 2084 1875 1087 1611 1344
+[3,]  447 1011 2865 2173  276
+[4,] 2690  200 1759 1423  460
+[5,]  373  844 2077 2175  869
+
+
+> cacheSolve( func_process )
+              [,1]         [,2]          [,3]          [,4]         [,5]
+[1,]  1.000000e+00 1.776357e-15 -2.664535e-15  0.000000e+00 4.440892e-16
+[2,]  4.440892e-16 1.000000e+00 -1.776357e-15  8.881784e-16 0.000000e+00
+[3,]  1.776357e-15 0.000000e+00  1.000000e+00  0.000000e+00 0.000000e+00
+[4,] -5.329071e-15 0.000000e+00  0.000000e+00  1.000000e+00 7.105427e-15
+[5,]  1.776357e-15 0.000000e+00 -7.105427e-15 -1.421085e-14 1.000000e+00
+
+
+> inverso <- cacheSolve( func_process )
+
+> inverso %*% x
+     [,1] [,2] [,3] [,4] [,5]
+[1,] 1249 2951  981  441  122
+[2,] 2084 1875 1087 1611 1344
+[3,]  447 1011 2865 2173  276
+[4,] 2690  200 1759 1423  460
+[5,]  373  844 2077 2175  869
+
+
+
+
+
+
+
+> func_process$get()   
+     [,1] [,2] [,3] [,4] [,5]
+[1,]    5   10   13    6    3
+[2,]   24   16   12   15    7
+[3,]    8   22    2    4   21
+[4,]    1   18   14   20   11
+[5,]   19   17   23   25    9
+
+> cacheSolve( func_process )
+              [,1]          [,2]          [,3]          [,4]          [,5]
+[1,]  1.000000e+00  0.000000e+00  0.000000e+00  1.110223e-16  0.000000e+00
+[2,]  0.000000e+00  1.000000e+00  0.000000e+00  0.000000e+00 -4.440892e-16
+[3,]  0.000000e+00  2.220446e-16  1.000000e+00  0.000000e+00  1.110223e-16
+[4,] -1.804112e-16 -2.775558e-16 -2.498002e-16  1.000000e+00 -1.734723e-16
+[5,]  4.440892e-16 -4.440892e-16  0.000000e+00 -4.440892e-16  1.000000e+00
+
+> inverso <- cacheSolve( func_process )
+
+> inverso %*% x
+     [,1] [,2] [,3] [,4] [,5]
+[1,]    5   10   13    6    3
+[2,]   24   16   12   15    7
+[3,]    8   22    2    4   21
+[4,]    1   18   14   20   11
+[5,]   19   17   23   25    9
 
 
 //
