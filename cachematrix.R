a057d6c9e8fc97d9fd677169a99ff55744442e76
dcialdella <dcialdella@gmail.com>
2016-06-14 15:26:48 +0200

    4 hours working on it

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 
+##
 ## 2 functions for Asignments Number 2
 ## R programming course U.J.H.
 ## DAC 2016-06-14
@@ -6,17 +7,20 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 ##    copy / paste from orig page
+## x - matrix used in function
+## m - inversed matrix.
+## 
 makeCacheMatrix <- function( x = matrix() ) 
 {
   m <- NULL
 
-  set<-function( y )
+  set<-function( y ) # set x with matrix  and  blank to Imatrix (m)
   {
-    x <<- y
-    m <<- NULL
+    x <- y     
+    m <- NULL
   }
 
-  get <- function() 
+  get <- function()   # return matrix passed to function (x)
   {
     x
   }
@@ -25,11 +29,12 @@ makeCacheMatrix <- function( x = matrix() )
   {
   }
 
-  getInverse <- function()
+  getInverse <- function()  # return the inverse 
   {
+  	m
   }
 
-  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
@@ -42,30 +47,51 @@ makeCacheMatrix <- function( x = matrix() )
 
 cacheSolve <- function(x, ...) 
 {
-  ## Return a matrix that is the inverse of 'x'
-
+  m <- x$getInverse()
+  if ( is.null( m ) ) 
+  {
+    temporal <- x$get()
+    m        <- solve(temporal) %*% temporal
+    x$setInverse(m)
+  }
+  m # return matrix inverted
 }
 
 
 
+
+
 //
+
 How test it
 
 # Create a sample matrix (easy)
   set.seed(25) # funny number for this sample
-  x <- matrix( sample(1:25), 5,5)
+  x <- matrix( sample(3000), 5, 5)
   func_process <- makeCacheMatrix(x)
   func_process$get()   # Read cachematrix complete
+  cacheSolve( func_process )
+  inverso <- cacheSolve( func_process )
+  inverso %*% x
 
-# Create Another matrix for testing
+
+# Create Another matrix for testing 
   set.seed(52)
-  x <- matrix( sample(1:25), 5,5)
+  x <- matrix( sample(1:25), 5, 5)
   func_process <- makeCacheMatrix(x)
   func_process$get()   # Read cachematrix complete
+  cacheSolve( func_process )
+  inverso <- cacheSolve( func_process )
+  inverso %*% x
 
+# Create Another matrix for testing 
+  x <- matrix( 1:25, 5, 5)
+  func_process <- makeCacheMatrix(x)
+  func_process$get()   # Read cachematrix complete
+  cacheSolve( func_process )    ## WARNING SINGULAR 
+  inverso <- cacheSolve( func_process )
+  inverso %*% x
 
-  func_process$
-cX$get()
-cacheSolve(cX)
 
 //
+
